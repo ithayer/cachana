02c2ad267a91a3ef370da4d8eecfcb6b8f19e021
ignacio <ignacio@minnow.(none)>
2011-10-29 21:58:22 -0700

    Initial stuff.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject cachana "1.0.0-SNAPSHOT"
+(defproject cachana "0.1.0-SNAPSHOT"
   :description "Cachana makes it easy to memoize function calls in clojure to memcache."
   :repositories {"spy" "http://files.couchbase.com/maven2/"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [spy/spymemcached "2.7.3"]]
+                 [spy/spymemcached "2.7.3"]
+                 [clj-json "0.3.2"]]
   :main cachana.core)
+
--- src/cachana/internal/connection.clj
+++ src/cachana/internal/connection.clj
@@ -1,5 +1,6 @@
 (ns cachana.internal.connection
-  (:require [clojure.string :as str])
+  (:require [clojure.contrib.accumulators :as acc]
+            [clojure.string :as str])
   (:import [net.spy.memcached
             MemcachedClient
             ConnectionFactoryBuilder
@@ -8,28 +9,77 @@
            [net.spy.memcached.auth
             AuthDescriptor
             PlainCallbackHandler]
-           [java.net InetSocketAddress]))
+           [java.net InetSocketAddress])
+  (:use [cachana.config :only [options]]))
 
 (def client (atom nil))
 
-(defn- build
+(def stats (atom {}))
+
+(def ^{:dynamic true} *thread-local-cache* nil)
+
+(defn- create-factory
   "Use the PLAIN auth descriptor and build the connection factory."
   [user password]
-  (let [auth (AuthDescriptor. (into-array ["PLAIN"]) (PlainCallbackHandler. user password))]
+  (let [auth (AuthDescriptor. (into-array ["PLAIN"])
+                              (PlainCallbackHandler. user password))]
     (.. (ConnectionFactoryBuilder.)
         (setProtocol ConnectionFactoryBuilder$Protocol/BINARY)
         (setAuthDescriptor auth)
         build)))
 
-(defn memcached! [& {:keys [host port user password] :or {host "localhost", port 11211, user "", password ""}}]
-  (reset! mcd (if (and (str/blank? user) (str/blank? password))
-                (MemcachedClient. (list (InetSocketAddress. host port)))
-                (MemcachedClient.
-                  (make-connection-factory user password)
-                  (AddrUtil/getAddresses (str host ":" port))))))
+(defn- initialize-client!
+  [& {:keys [host port user password] :as options}]
+  (let [host (or host "localhost")
+        port (if (not (nil? port)) (Integer. port) 11211)]
+    (reset! client (if (and (nil? user) (nil? password))
+                     (MemcachedClient. (list (InetSocketAddress. host port)))
+                     (MemcachedClient.
+                      (create-factory user password)
+                      (AddrUtil/getAddresses (str host ":" port)))))))
 
 (defn cache-set [key value & {:keys [expiration] :or {expiration 3600}}]
-  (.set @mcd (key->str key) expiration value))
+  (.set @client (name key) expiration value))
 
 (defn cache-get [key & {:keys [default] :or {default nil}}]
-  (if-let [res (.get @mcd (key->str key))] res default))
+  (if-let [res (.. @client (get (name key)))] res default))
+
+(defn inc-stat [& ks]
+  (swap! stats update-in ks (fn [x] (inc (or x 0)))))
+
+(defn try-local [joined-key]
+  (when *thread-local-cache*
+    (get @*thread-local-cache* joined-key)))
+
+(defn set-local! [joined-key val]
+  (when *thread-local-cache*
+    (swap! *thread-local-cache* assoc-in [joined-key] val)
+    (println @*thread-local-cache*)
+    val))
+
+(defmacro with-local-cache [& body]
+  `(binding [*thread-local-cache* (atom {})]
+     ~@body))
+
+(defmacro with-caching [[bucket key] & body]
+  (let [[pack-fn unpack-fn] (get @options :serialization)]
+    `(let [key# (str ~(name bucket) ~(get @options :bucket-separator) ~key)
+           in-local-cache?# (~try-local key#)]
+       (if in-local-cache?#
+         (do (~inc-stat ~bucket :hit :local)
+             in-local-cache?#)
+         (do (and *thread-local-cache* (~inc-stat ~bucket :miss :local))
+             (if-let [in-network-cache?# (~cache-get key#)]
+               (do (~inc-stat ~bucket :hit :network)
+                   (set-local! key# (~unpack-fn in-network-cache?#)))
+               (do (~inc-stat ~bucket :miss :network)
+                   (let [result# ~@body]
+                     (~cache-set key# (~pack-fn result#))
+                     (~set-local! key# result#)
+                     result#))))))))
+  
+(with-caching [:accounts user-id] (reduce + (range 0 10)))
+  
+  
+   
+         
